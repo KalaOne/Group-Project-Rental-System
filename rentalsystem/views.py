10567ade07261c520468b01623414d37d9a32e46
M Taylor <ecstatic@gmail.com>
2019-03-25 15:11:29 +0000

    Added basic category view

--- rentalsystem/views.py
+++ rentalsystem/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.template.response import TemplateResponse
+from django.utils import timezone
 from django.http import HttpResponseRedirect
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-from .forms import CustomUserCreationForm
+from .forms import *
 
 
 def index(request):
@@ -17,3 +16,12 @@ class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
+def add_category(request):
+    cat_objects = Category.objects.all().values()   # get all the category objects from model
+
+    print(cat_objects)
+
+    # render template, give it all the category objects
+    return render(request, 'add_category.html', {'cat_objects': cat_objects})
