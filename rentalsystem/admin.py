2ad5c68a397fd506953c6877976c31e59dc5494f
M Taylor <ecstatic@gmail.com>
2019-03-26 13:49:15 +0000

    Updated admin page for users, made Customer default

--- rentalsystem/admin.py
+++ rentalsystem/admin.py
@@ -9,7 +9,19 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username', 'role']
+    list_display = ['email', 'username', 'role', 'region', 'name', 'job_capacity',
+                    'card_long_number', 'card_expiry_date', 'card_cvv']
+    fieldsets = (
+        ('Basic information', {
+            'fields': ('email', 'username', 'role', 'region', 'name')
+        }),
+        ('Customer payment information', {
+            'fields': ('card_long_number', 'card_expiry_date', 'card_cvv')
+        }),
+        ('Staff information', {
+            'fields': ('job_capacity',)
+        }),
+    )
 
 
 # sets up the category view in the admin page
