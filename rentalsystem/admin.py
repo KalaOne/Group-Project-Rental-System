c5f6d690b8d53fe37a3b160dd7a546c5aa43f186
M Taylor <ecstatic@gmail.com>
2019-03-26 12:42:28 +0000

    Category changes done in admin

--- rentalsystem/admin.py
+++ rentalsystem/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
+from rentalsystem import models
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
@@ -9,6 +9,15 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username',]
+    list_display = ['email', 'username', 'role']
 
+
+# sets up the category view in the admin page
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description']
+    fields = ('title', 'description')
+
+
+# register the models with the admin site
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(models.Category, CategoryAdmin)
