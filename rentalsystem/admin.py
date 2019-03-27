7c7e93325c89f8204f3c8e84fb3cf0b5f4c421d1
M Taylor <ecstatic@gmail.com>
2019-03-27 09:39:26 +0000

    Added Admin page for Items

--- rentalsystem/admin.py
+++ rentalsystem/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from rentalsystem import models
 from django.contrib.auth.admin import UserAdmin
-
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from .models import CustomUser, ItemCategoryPair
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
@@ -29,7 +28,22 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'description']
     fields = ('title', 'description')
 
+# sets up the inline admin edit for ItemCategoryPair 
+class ItemCategoryPairInline(admin.StackedInline):
+    model = ItemCategoryPair
+    list_display = ['']
+
+# sets up the item view in the admin page
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'info']
+    fields = ('name', 'info')
+
+    # inlines ability to change different model from this one
+    inlines = [ItemCategoryPairInline]
+
+
 
 # register the models with the admin site
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.Item, ItemAdmin)
