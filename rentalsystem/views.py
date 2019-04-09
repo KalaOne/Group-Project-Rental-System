388cbfdf9fdc1a781ae71679e1681737a56920c1
M Taylor <ecstatic@gmail.com>
2019-04-09 15:07:36 +0100

    Button added to allow automatic completion of jobs

--- rentalsystem/views.py
+++ rentalsystem/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django.shortcuts import render, redirect
 from django.template.response import TemplateResponse
 from django.utils import timezone
@@ -26,8 +27,23 @@ def landing(request):
     return render(request, "rentalsystem/landing.html")
 
 
-
 def myjobs(request):
+    # if update button pressed
+    if request.method == 'POST':
+        # get job id from the post request (button press)
+        jobpk = request.POST.get('jobpk')
+
+        # get the job from the model using the primary key
+        job = Job.objects.get(id = jobpk)
+
+        # set datetime and boolean delivery status
+        job.delivered_datetime = datetime.datetime.now()
+        job.delivered = True
+
+        # save object
+        job.save()
+        
+    # create list of current users jobs to pass into template
     current_id = request.user.id # gets current logged in staff ID
     jl_id = JobList.objects.get(staff_id = current_id) #get current staff joblist
     jobs = Job.objects.filter(job_list_id = jl_id) #get all jobs for joblist_id
