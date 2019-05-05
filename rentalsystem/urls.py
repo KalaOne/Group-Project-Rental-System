5f99413a82eba4f23320653bb8e5ba8b4ed84ca5
M Taylor <ecstatic@gmail.com>
2019-05-05 21:32:10 +0100

    Started writing unit tests, included coverage package

--- rentalsystem/urls.py
+++ rentalsystem/urls.py
@@ -11,12 +11,9 @@ urlpatterns = [
     path('myjobs/', views.myjobs, name='myjobs'),
     path('myjobs/jobcomp/', views.myjobs, name='myjobsupdate'),
     path('profile/', views.profile, name='profile'),
-
-
     path('home_search/', views.home_search),
     path('jobstats/', views.jobstats, name='jobstats'),
     path('jobstats/jobstatsregion/', views.jobstats, name='jobstatsregion'),
     path('postItem/', views.UserPostItem.as_view(), name='postItem'),
     path('post_item_details/<int:pk>', views.item_details, name="post_item_details"),
-
 ]
