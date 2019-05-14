ec5f6328c758766517e33b1332719d4e50f46dab
George Marshall-Dutton <gprmd62@gmail.com>
2019-05-14 16:07:58 +0100

    Transaction between seller and Admin created on posting a new item listing. Job created at this point linked to transaction

--- rentalsystem/models.py
+++ rentalsystem/models.py
@@ -93,7 +93,7 @@ class Transaction(models.Model):
     renter_id = models.ForeignKey(CustomUser, on_delete=models.PROTECT, related_name='renter_id')
     total_cost = models.PositiveIntegerField()
     start_date = models.DateField()
-    end_date = models.DateField()
+    end_date = models.DateField(blank = True, null = True)
 
 
 # job list class
@@ -103,7 +103,7 @@ class JobList(models.Model):
 
 # job class
 class Job(models.Model):
-    transaction_id = models.ForeignKey(Transaction, on_delete=models.PROTECT, db_column='transaction_id')
+    transaction_id = models.ForeignKey(Transaction, on_delete=models.PROTECT, db_column='transaction_id', null=True)
     job_list_id = models.ForeignKey(JobList, on_delete=models.PROTECT, db_column='job_list_id', blank=True, null=True)
     due_delivery_datetime = models.DateTimeField()
     delivered_datetime = models.DateTimeField(blank=True, null=True)
